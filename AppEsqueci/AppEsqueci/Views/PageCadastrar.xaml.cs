95a413e0847f25fe8ec96a1362e8fdc8e8e43d05
Vinicius Nogueira <vinicius.nogueira@unidesk.com.br>
2020-01-13 16:03:26 -0300

    Cadastrar um item pela interface

--- AppEsqueci/AppEsqueci/Views/PageCadastrar.xaml.cs
+++ AppEsqueci/AppEsqueci/Views/PageCadastrar.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppEsqueci.Models;
+using AppEsqueci.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,38 @@ namespace AppEsqueci.Views
         public PageCadastrar()
         {
             InitializeComponent();
+        }
+
+        private void Button_pagecadastrar_salvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelNotas nota = new ModelNotas();
+                nota.Titulo = entry_pagecadastrar_titulo.Text;
+                nota.Dados = editor_pagecadastrar_dados.Text;
+                nota.Favorito = swFavorito.IsToggled;
+
+                ServicesDBNotas dbNotas = new ServicesDBNotas(App.DbPath);
+
+                if (button_pagecadastrar_salvar.Text == "Inserir")
+                {
+                    dbNotas.Inserir(nota);
+                    DisplayAlert("Resultado da operação: ", dbNotas.StatusMessage, "OK");
+                }
+                else //Alterar
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private void Button_pagecadastrar_cancelar_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
