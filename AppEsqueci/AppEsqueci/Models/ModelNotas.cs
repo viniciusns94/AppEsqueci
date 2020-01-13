4da52d7c9612b6560577d1931fd318668ee922c2
Vinicius Nogueira <vinicius.nogueira@unidesk.com.br>
2020-01-13 09:21:43 -0300

    Inclusão do primeiro model, e criação da primeira tabela (Anotacoes)

---
+++ AppEsqueci/AppEsqueci/Models/ModelNotas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppEsqueci.Models
+{
+    [Table("Anotacoes")]
+    public class ModelNotas
+    { 
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public string Titulo { get; set; }
+        [NotNull]
+        public string Dados { get; set; }
+        [NotNull]
+        public bool Favorito { get; set; }
+    }
+}
