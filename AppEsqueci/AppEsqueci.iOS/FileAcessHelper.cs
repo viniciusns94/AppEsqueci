aabbc2e30b0833b52ef6666fd6a40a99d6ffd0ae
Vinicius Nogueira <vinicius.nogueira@unidesk.com.br>
2020-01-13 09:37:12 -0300

    Classes auxliares criadas em cada um dos projetos para acessorar a comunicação com o app

---
+++ AppEsqueci/AppEsqueci.iOS/FileAcessHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace AppEsqueci.iOS
+{
+    class FileAcessHelper
+    {
+        public static string GeLocalFilePath(string filename)
+        {
+            string docFolder = Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string libFolder = System.IO.Path.Combine(docFolder, "..", "Library");
+            if (!System.IO.Directory.Exists(libFolder))
+            {
+                System.IO.Directory.CreateDirectory(libFolder);
+            }
+            return System.IO.Path.Combine(libFolder, filename);
+        }
+    }
+}
